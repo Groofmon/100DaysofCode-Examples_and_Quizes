c722266dc1d561f468771d21f13f619d7aebeaf9
İ. Enes Aktaşoğlu <120311002+Groofmon@users.noreply.github.com>
2024-01-24 16:18:56 +0300

    2nd Day - Tip Calculator
    This project covers rounding of floating-point numbers

---
+++ Python Files/2.Tip_Calculator.py
@@ -0,0 +1,10 @@
+print()
+
+print("Welcome to the tip calculator.")
+bill = float(input("What was the total bill? $"))
+tip_perct = float(input("What percentage you would like to give? 10, 12, 15, or how much you want? %"))
+people_count = int(input("How many people to split the bill? "))
+
+result_per_person = round( ( bill*(100+tip_perct)/100 ) / people_count , 2) 
+
+print(f"Each person should pay ${result_per_person}.")  
